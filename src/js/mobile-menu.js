cd0ac49ca78d8c72cd60c627f16c3df8caa63595
suprunivss <suprunivss@gmail.com>
2021-08-11 18:40:21 +0300

    feat: create small screen

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,15 +1,15 @@
 (function () {
   const openButton = document.querySelector('.header__burger-btn');
-  const menu = document.querySelector('.mobile__menu');
+  const menu = document.querySelector('.mobile-menu');
 
   function closeMenuHandler(event) {
-    const isBurgerMenu = event.target.closest('.mobile__menu');
-    const isCloseButton = event.target.closest('.mobile__button');
+    const isBurgerMenu = event.target.closest('.mobile-menu');
+    const isCloseButton = event.target.closest('.mobile-menu__button');
 
     if (isBurgerMenu && !isCloseButton) return;
 
     openButton.classList.remove('header__burger-btn_active');
-    menu.classList.remove('mobile__menu_active');
+    menu.classList.remove('mobile-menu_active');
 
     document.removeEventListener('click', closeMenuHandler);
   }
@@ -18,7 +18,7 @@
     event.stopPropagation();
 
     openButton.classList.toggle('header__burger-btn_active');
-    menu.classList.toggle('mobile__menu_active');
+    menu.classList.toggle('mobile-menu_active');
 
     document.addEventListener('click', closeMenuHandler);
   });
