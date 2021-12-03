9c2aba9f6831595eedf0236c61c7d04ed3904328
Martusin <suprunivss@gmail.com>
2021-12-03 20:57:47 +0300

    feat: Create webpack config

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    home: ['@babel/polyfill', './src/js/index.js'],
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './public',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Большой театр',
+      favicon: path.resolve(__dirname, 'src/assets/img/favicon.png'),
+      filename: 'home.html',
+      template: './src/index.html',
+      minify: true,
+    }),
+  ],
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'public'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+
+    ],
+  },
+};
